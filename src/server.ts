780988453c35460af8c1b94c9b7c2e231a1dee5b
KoyamaSohei <koyamaso0309@gmail.com>
2020-01-07 02:40:50 +0900

    change: listen host

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,6 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`express listening on ${port}`);
 })
